2394562442280ddc746f0acd78cd6bc02b65fbaf
julienmetivier <julienmetivier5@hotmail.com>
2014-04-14 09:18:50 -0700

    petit update des etoiles

--- modele.py
+++ modele.py
@@ -1,3 +1,21 @@
+import random
+from enum import Enum
+
+class TypeEtoile(Enum):
+   	mereHumain = 1
+    mereGubru = 2
+    mereCzin = 3
+    indep = 4
+    humain = 5
+    gubru = 6
+    czin = 7
+
+class NiveauIntelligence(Enum):
+    aucun = 1
+    premier = 2
+    deuxieme = 3
+    troisieme = 4
+
 class Modele():
     def __init__(self, parent):
         self.parent = parent
@@ -20,7 +38,19 @@ class Gubru():
     pass
 
 class Etoile():
-    pass
+    def __init__(self, typeEtoile):
+    	self.niveauIntelligence = 0
+    	self.nombreUsine = None
+    	self.quantiteVaisseau = None
+    	self.typeEtoile = typeEtoile
+    	construireUsine()
+
+    def construireUsine(self):
+    	if self.typeEtoile == TypeEtoile.mereHumain or self.typeEtoile == TypeEtoile.mereCzin or self.typeEtoile == TypeEtoile.mereGubru :
+    		self.nombreUsine = 10
+    		self.quantiteVaisseau = 100
+    		self.niveauIntelligence = NiveauIntelligence.troisieme
+
 
 class Flotte():
     pass
