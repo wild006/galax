23dbba4a454f7c2faef763e2197109cde3528841
mathiaspg <mathsniki@hotmail.com>
2014-04-27 17:13:33 -0400

    probleme dans self.parent.Humain.deplacementFlotte

--- vue.py
+++ vue.py
@@ -53,6 +53,7 @@ class Vue():
 		self.cadreInfo.grid(column=1,row=0,sticky=N)
 		#self.cadreInfo.grid(column=2,row=0)#appelle des functions pour resultat 
 		self.cadreCommande.grid(column=1,row=1,columnspan=2)
+		
 		self.canevas=Canvas(self.cadreJeu,width=800,height=800,bg="black")
 		
 		self.backgroundCanevas=Image.open("background.jpg")#Agrandir selon la fenetre
@@ -60,8 +61,11 @@ class Vue():
 		self.canevas.create_image(0,0,image=self.background_image,anchor=NW)
 		self.canevas.pack()
 		
+		
 		self.sliderDeplacement = Scale(self.cadreFlotte,orient=HORIZONTAL,length=400,width=20,sliderlength=10,from_=0,to=0)
 		self.sliderDeplacement.pack()
+		
+		
 		
 		self.logoGalax = Image.open("logo.jpg") 
 		self.photoGalax = ImageTk.PhotoImage(self.logoGalax)
@@ -130,6 +134,9 @@ class Vue():
 		self.canevas.bind("<Button-1>", self.clickCanevas)
 		self.labelTemps = Label(self.cadreCommande,text=self.parent.getTemps(),relief=GROOVE,width=15)
 		self.labelTemps.pack()
+		
+		
+		
 		b1=Button(self.cadreFlotte,text="Faire D\xE9placement")
 		b1.pack()
 		
@@ -137,6 +144,8 @@ class Vue():
 		b2.pack()
 		b3=Button(self.cadreCommande,text="Quitter",width=12,command=self.initFermer)
 		b3.pack()
+		
+		
 		
 		#self.canevas.create_oval(0, 0, 100, 100)
 		#print(self.parent.getListeEtoile())
@@ -187,8 +196,14 @@ class Vue():
 		except:
 			pass
 		if etoile==None :
-			self.etoileDepart=None
+			self.etoileDepart=None#detecte toujours a nul
 			self.etoileArrivee=None
+			self.labelEtoileProprioResultat.config(text="")
+			self.labelEtoileVaisseauResultat.config(text="")
+			self.labelEtoileManuResultat.config(text="")
+			self.labelDestinationProprioResultat.config(text="")
+			self.labelDestinationManuResultat.config(text="")
+			self.labelDestinationVaisseauResultat.config(text="")
 			#verifier si etoileDepart est = a none
 		elif etoile.typeEtoile==1 or etoile.typeEtoile==5:
 			if self.etoileDepart == None:#premier click
@@ -200,9 +215,9 @@ class Vue():
 			self.labelEtoileProprioResultat.config(text= "Humain")
 			self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
 			self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
-			if self.etoileArrivee==None:
-				self.etoileDepart=None
-			elif self.etoileArrivee==self.etoileDepart:
+			#if self.etoileArrivee==None:
+			#	self.etoileDepart=None
+			if self.etoileArrivee==self.etoileDepart:
 				self.etoileArrivee=None
 			elif self.etoileArrivee.typeEtoile==1 or self.etoileArrivee.typeEtoile==5:
 				self.parent.Humain.deplacementFlotte(self.etoileDepart,self.etoileArrivee,self.sliderDeplacement.get())
@@ -210,73 +225,89 @@ class Vue():
 				
 		elif etoile.typeEtoile==2 or etoile.typeEtoile==6 :
 			self.labelEtoileProprioResultat.config(text= "Gubru")
-			if etoile.nombreVaisseau==None:
-				self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
-			elif etoile.nombreVaisseau==0:
-				self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
+			if self.etoileDepart !=None:#verifier si on a clicker sur une etoile humaine en premier
+				self.etoileArrivee=etoile
+				self.parent.Humain.deplacementFlotte(self.etoileDepart,self.etoileArrivee,self.sliderDeplacement.get())
+				self.labelDestinationProprioResultat.config(text="Gubru")
+				if etoile.nombreUsine==None:
+					self.labelDestinationManuResultat.config(text="Aucune Info.")
+				else:
+					self.labelDestinationManuResultat.config(text=etoile.nombreUsine)
+				if etoile.nombreVaisseau==None or etoile.nombreVaisseau==0:
+					self.labelDestinationVaisseauResultat.config(text="Aucune Info.")
+				else:
+					self.labelDestinationVaisseauResultat.config(text=etoile.nombreVaisseau)
 			else:
-				self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
-				
-			if etoile.nombreUsine==None:
-				self.labelEtoileManuResultat.config(text="Aucune Info.")
-			else:
-				self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
+				if etoile.nombreVaisseau==None or etoile.nombreVaisseau==0:
+					self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
+				else:
+					self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
+					
+				if etoile.nombreUsine==None:
+					self.labelEtoileManuResultat.config(text="Aucune Info.")
+				else:
+					self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
 			#self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)#niveau d'intelligence
 			#self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
-			if self.etoileDepart !=None:#verifier si on a clicker sur une etoile humaine en premier
-				self.labelDestinationProprioResultat.config(text=etoile.typeEtoile)
-				self.labelDestinationManuResultat.config(text=etoile.nombreUsine)
-				self.labelDestinationVaisseauResultat.config(text=etoile.nombreVaisseau)
+			
 			#self.etoileDepart=None
 			#self.etoileArrivee=None
 			
 			
 		elif etoile.typeEtoile==3 or etoile.typeEtoile==7:
 			self.labelEtoileProprioResultat.config(text= "Czin")
-			if etoile.nombreVaisseau==None:
-				self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
-			elif etoile.nombreVaisseau==0:
-				self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
-			else:
-				self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
-				
-			if etoile.nombreUsine==None:
-				self.labelEtoileManuResultat.config(text="Aucune Info.")
-			else:
-				self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
 			if self.etoileDepart !=None:#verifier si on a clicker sur une etoile humaine en premier
-				self.labelDestinationProprioResultat.config(text=etoile.typeEtoile)
-				self.labelDestinationManuResultat.config(text=etoile.nombreUsine)
-				self.labelDestinationVaisseauResultat.config(text=etoile.nombreVaisseau)
+				self.etoileArrivee=etoile
+				self.parent.Humain.deplacementFlotte(self.etoileDepart,self.etoileArrivee,self.sliderDeplacement.get())
+				self.labelDestinationProprioResultat.config(text="Czin")
+				if etoile.nombreUsine==None:
+					self.labelDestinationManuResultat.config(text="Aucune Info.")
+				else:
+					self.labelDestinationManuResultat.config(text=etoile.nombreUsine)
+				if etoile.nombreVaisseau==None or etoile.nombreVaisseau==0:
+					self.labelDestinationVaisseauResultat.config(text="Aucune Info.")
+				else:
+					self.labelDestinationVaisseauResultat.config(text=etoile.nombreVaisseau)
+			else:
+				if etoile.nombreVaisseau==None or etoile.nombreVaisseau==0:
+					self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
+				else:
+					self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
+				
+				if etoile.nombreUsine==None:
+					self.labelEtoileManuResultat.config(text="Aucune Info.")
+				else:
+					self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
+			
 			#self.etoileDepart=None
 			#self.etoileArrivee=None
 			
 			
 		elif etoile.typeEtoile==4:
 			self.labelEtoileProprioResultat.config(text= "Ind\xE9pendant")
-			if etoile.nombreVaisseau==None:
-				self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
-			elif etoile.nombreVaisseau==0:
-				self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
-			else:
-				self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
-				
-			if etoile.nombreUsine==None:
-				self.labelEtoileManuResultat.config(text="Aucune Info.")
-			else:
-				self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
 			if self.etoileDepart !=None:#verifier si on a clicker sur une etoile humaine en premier
-				self.labelDestinationProprioResultat.config(text=etoile.typeEtoile)
-				self.labelDestinationManuResultat.config(text=etoile.nombreUsine)
-				self.labelDestinationVaisseauResultat.config(text=etoile.nombreVaisseau)
-			#self.etoileDepart=None
-			#self.etoileArrivee=None
+				self.etoileArrivee=etoile
+				self.parent.Humain.deplacementFlotte(self.etoileDepart,self.etoileArrivee,self.sliderDeplacement.get())
+				self.labelDestinationProprioResultat.config(text="Ind\xE9pendant")
+				if etoile.nombreUsine==None:
+					self.labelDestinationManuResultat.config(text="Aucune Info.")
+				else:
+					self.labelDestinationManuResultat.config(text=etoile.nombreUsine)
+				if etoile.nombreVaisseau==None or etoile.nombreVaisseau==0:
+					self.labelDestinationVaisseauResultat.config(text="Aucune Info.")
+				else:
+					self.labelDestinationVaisseauResultat.config(text=etoile.nombreVaisseau)
+			else:
+				if etoile.nombreVaisseau==None or etoile.nombreVaisseau==0:
+					self.labelEtoileVaisseauResultat.config(text="Aucune Info.")
+				else:
+					self.labelEtoileVaisseauResultat.config(text=etoile.nombreVaisseau)
+					
+				if etoile.nombreUsine==None:
+					self.labelEtoileManuResultat.config(text="Aucune Info.")
+				else:
+					self.labelEtoileManuResultat.config(text=etoile.nombreUsine)
 			
-				
-		#A FAIRE CHANGER LE FOCUS DEPLACEMNT (DESELECTION D'UNE ETOILE)
-		#x = self.canevas.canvasx(event.x)
-		#y = self.canevas.canvasy(event.y)
-	   
 	
 			
 		#frame = Frame(root, width=100, height=100)
@@ -285,6 +316,13 @@ class Vue():
 		
 	def initOption(self):
 		self.canevas=Canvas(self.root,width=800,height=600,bg="white")
+		
+		self.background=Image.open("background.jpg")#Agrandir selon la fenetre
+		self.background_images=ImageTk.PhotoImage(self.background)
+		self.background_label = Label(self.canevas, image=self.background_images)
+		self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+		
+		
 		self.logoGalax = Image.open("logo.jpg") 
 		self.photoGalax = ImageTk.PhotoImage(self.logoGalax)
 		self.labelLogo = Label(self.canevas,image=self.photoGalax)
@@ -309,17 +347,12 @@ class Vue():
 		
 	def initHighScore(self):
 		self.cadrePartieHighScore=Frame(self.root)
-		#=======================================================================
-		# self.cadreHighScore=Frame(self.cadrePartieHighScore)
-		# self.cadreNom=Frame(self.cadrePartieHighScore)
-		# self.cadreTitre=Frame(self.cadrePartieHighScore)
-		# self.cadreQuitter=Frame(self.cadrePartieHighScore)
-		# self.cadreTitre.grid(column=1,row=0)
-		# self.cadreNom.grid(column=0,row=1)
-		# self.cadreHighScore.grid(column=2,row=1)
-		# self.cadreQuitter.grid(column=1,row=2)
-		#=======================================================================
-		#self.canevas=Canvas(self.cadrePartieHighScore,width=800,height=600,bg="white")
+
+		self.background=Image.open("background.jpg")#Agrandir selon la fenetre
+		self.background_images=ImageTk.PhotoImage(self.background)
+		self.background_label = Label(self.cadrePartieHighScore, image=self.background_images)
+		self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+		
 		self.logoGalax = Image.open("logo.jpg") 
 		self.photoGalax = ImageTk.PhotoImage(self.logoGalax)
 		self.labelLogo = Label(self.cadrePartieHighScore,image=self.photoGalax)
@@ -357,11 +390,16 @@ class Vue():
 		self.root.destroy()
 		
 	def initMenu(self):#Petit bug pour le retour au menu principal
-		#self.canevas.pack_forget()
+		
 		try:
 			self.cadrePartieHighScore.pack_forget()
+			self.labelLogo.pack_forget()
 		except:
-			pass #si on a pas packer highscore...
+			try:
+				self.canevas.pack_forget()
+			except:
+				pass #si on a pas packer highscore...
+	
 		self.cadreLobby.pack()
 		#self.initLobby()
 		
